d64e33e718917c276fbae33d7c07c8187c4e4d8d
Jakub Maternowski <jakub.mat977@gmail.com>
2018-02-19 08:45:03 +0100

    dded new function

--- cppRepProject/main.cpp
+++ cppRepProject/main.cpp
@@ -5,5 +5,22 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
+
+
+    int number1
+    number1 = readNumber();
+    cout << "a number entered" <<  endl;
+
+
     return 0;
+
+}
+
+int readNumber()
+{
+    cout << "gimme number ";
+    int number;
+
+    cin >> number;
+    return number;
 }
